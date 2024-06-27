40bb19817a2caf711079e9ae26a5c3a4d1e384be
jinhojang6 <jinho5051@gmail.com>
2024-06-28 01:04:58 +0900

    chore: update SEO content

--- src/configs/site.configs.ts
+++ src/configs/site.configs.ts
@@ -1,6 +1,7 @@
 export const siteConfigs = {
-  title: 'PSC',
-  description: 'PSC project',
+  title: 'Parallel Society Congress | Shaping the Future of Human Governance',
+  description:
+    'In a swiftly evolving world, our conventional understanding of the nation state is increasingly challenged. Join global thought leaders, passionate founders and builders to design humanity’s future through a political and cultural shift backed by cutting-edge technologies.',
   keywords: [],
   xHandle: '',
   youtubeHandle: '',
