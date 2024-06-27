f43d32150dad1275686d6113bf1e6bd2f15a5317
jinhojang6 <jinho5051@gmail.com>
2024-06-28 00:00:38 +0900

    feat: implement favicon and og image

--- src/pages/api/og.tsx
+++ src/pages/api/og.tsx
@@ -6,70 +6,38 @@ export const config = {
   runtime: 'edge',
 }
 
+const image = fetch(new URL('/public/assets/og.jpg', import.meta.url)).then(
+  (res) => res.arrayBuffer(),
+)
+
 // Doc: https://vercel.com/docs/concepts/functions/edge-functions/og-image-generation/og-image-examples#using-a-local-image
 // Font: https://vercel.com/docs/functions/edge-functions/og-image-generation/og-image-examples#using-a-custom-font
 export default async function handler(request: NextRequest) {
-  const helvetica = await fetch(
-    new URL('../../../public/fonts/Helvetica.ttf', import.meta.url),
-  ).then((res) => res.arrayBuffer())
-  const { href } = request.nextUrl
-
-  const searchParams = new URL(href).searchParams
-  const pagePath = searchParams.get('pagePath') || '/'
+  const imageData = await image
 
   return new ImageResponse(
     (
       <div
         style={{
           display: 'flex',
-          flexDirection: 'column',
           width: '100%',
           height: '100%',
-          justifyContent: 'flex-start',
-          backgroundColor: '#fff',
-          color: '#1400ff',
-          position: 'relative',
-          fontFamily: 'Helvetica',
-          padding: '40px',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
         }}
       >
-        <div
-          style={{
-            display: 'flex',
-            flexDirection: 'column',
-            gap: '4px',
-          }}
-        >
-          <div
-            style={{
-              display: 'flex',
-              alignItems: 'flex-start',
-              fontSize: '54px',
-            }}
-          >
-            PSC
-          </div>
-
-          <div style={{ fontSize: '42px', marginTop: '20px' }}>
-            The future is a second enlightenment of the digital world.
-          </div>
-
-          {pagePath === '/' ? null : pagePath === '/media' ? (
-            <div style={{ fontSize: '50px', marginTop: '80px' }}>Media</div>
-          ) : null}
-        </div>
+        <img
+          width="1200"
+          height="630"
+          src={imageData as unknown as string}
+          alt="og-image"
+        />
       </div>
     ),
     {
       width: 1200,
       height: 630,
-      fonts: [
-        {
-          name: 'Helvetica',
-          data: helvetica,
-          style: 'normal',
-        },
-      ],
     },
   )
 }
