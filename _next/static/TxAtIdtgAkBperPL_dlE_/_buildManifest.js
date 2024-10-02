10eb0c5323320376c5d0cafa3a3703912ed62a39
Jenkins <jenkins@status.im>
2024-10-02 12:41:42 +0000

    Update documentation

--- _next/static/TxAtIdtgAkBperPL_dlE_/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/205-1553fb7ef62cd56c.js","static/chunks/pages/index-de1a8d9a675032dd.js"],"/_error":["static/chunks/pages/_error-77823ddac6993d35.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
