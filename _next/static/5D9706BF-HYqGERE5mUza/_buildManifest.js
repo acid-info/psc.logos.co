33ca1fa525f5aeaa06c8714907725d94dcdb63c0
Jenkins <jenkins@status.im>
2024-10-25 10:53:07 +0000

    Update documentation

--- _next/static/5D9706BF-HYqGERE5mUza/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/205-1553fb7ef62cd56c.js","static/chunks/pages/index-f7f69da20ce88b66.js"],"/_error":["static/chunks/pages/_error-77823ddac6993d35.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
