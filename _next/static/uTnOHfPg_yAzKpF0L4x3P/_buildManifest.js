c22544f546fb585b16e8aea5c1a6ea989879491d
Jenkins <jenkins@status.im>
2024-10-31 13:04:52 +0000

    Update documentation

---
+++ _next/static/uTnOHfPg_yAzKpF0L4x3P/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/205-1553fb7ef62cd56c.js","static/chunks/pages/index-ef54bb1c98360627.js"],"/_error":["static/chunks/pages/_error-77823ddac6993d35.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
